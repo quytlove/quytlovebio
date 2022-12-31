3fedb2347b8d6766aade61496242dd44db6502d7
ashley <vothuykg123@gmail.com>
2022-12-31 18:47:17 +0700

    1st

---
+++ assets/js/parallax.js
@@ -0,0 +1,16 @@
+(function() { // Add event listener
+    document.addEventListener("mousemove", parallax);
+    // Magic happens here
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.03}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.08}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        document.querySelector("body").style.backgroundPosition = x;
+    }
+
+})();
